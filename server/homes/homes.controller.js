085b7a5439af62bc0a29d2ab28e714b93d29de9f
maya25 <32893503+maya25@users.noreply.github.com>
2018-06-21 11:25:15 +0300

    Add files via upload

---
+++ server/homes/homes.controller.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const homesService = require('./services/homes.service');
+const animalsService = require('./services/animals.service');
+
+const router = express.Router();
+
+// READ
+// GET /homes - Get all homes
+router.get('/', homesService.getHomes);
+// Get /homes/{homeId} - Get home by ID
+router.get('/:homeId', homesService.getHome);
+// Get /homes/{homeId}/animals/ - Get All animals for home by ID
+router.get('/:homeId/animals/', animalsService.getAnimals);
+// Get /homes/{homeId}/animals/{animalId} - Get animal by ID in a specific home (by ID)
+router.get('/:homeId/animals/:animalId', animalsService.getAnimal);
+
+// CREATE
+router.post('/', homesService.addHome);
+router.post('/:homeId/animals/', animalsService.addAnimal);
+
+// UPDATE
+router.put('/:homeId', homesService.editHome);
+router.put('/:homeId/animals/:animalId', animalsService.editAnimal);
+
+// DELETE
+router.delete('/:homeId', homesService.deleteHome);
+router.delete('/:homeId/animals/:animalId', animalsService.deleteAnimal);
+
+module.exports = router;
